d9dbdd104af138c7bf9965fcc60c8675117cce5f
Liu Yuanjing <lauyuanjing@163.com>
2018-12-01 14:04:23 +0800

    add Gaussian Blur

--- ctpn/demo.py
+++ ctpn/demo.py
@@ -26,30 +26,53 @@ def resize_im(im, scale, max_scale=None):
 def draw_boxes(img, image_name, boxes, scale):
     hight, width = img.shape[0:2]
     base_name = image_name.split('\\')[-1]
-    # with open('data/results/' + 'res_{}.txt'.format(base_name.split('.')[0]), 'w') as f:
+    with open('data/results/' + 'res_{}.txt'.format(base_name.split('.')[0]), 'w') as f:
+        for box in boxes:
+            minY = min(int(box[1] / scale), int(box[3] / scale), int(box[5] / scale), int(box[7] / scale))
+            if minY > 0 * (hight/scale) and box[8] >= 0.8:
+                if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
+                    continue
+                # if box[8] >= 0.9:
+                #     color = (0, 255, 0)
+                # elif box[8] >= 0.8:
+                #     color = (255, 0, 0)
+                color = (0, 255, 0)
+                cv2.line(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), color, 2)
+                cv2.line(img, (int(box[0]), int(box[1])), (int(box[4]), int(box[5])), color, 2)
+                cv2.line(img, (int(box[6]), int(box[7])), (int(box[2]), int(box[3])), color, 2)
+                cv2.line(img, (int(box[4]), int(box[5])), (int(box[6]), int(box[7])), color, 2)
+
+                min_x = min(int(box[0] / scale), int(box[2] / scale), int(box[4] / scale), int(box[6] / scale))
+                min_y = min(int(box[1] / scale), int(box[3] / scale), int(box[5] / scale), int(box[7] / scale))
+                max_x = max(int(box[0] / scale), int(box[2] / scale), int(box[4] / scale), int(box[6] / scale))
+                max_y = max(int(box[1] / scale), int(box[3] / scale), int(box[5] / scale), int(box[7] / scale))
+
+                line = ','.join([str(min_x), str(min_y), str(max_x), str(max_y)]) + '\r\n'
+                f.write(line)
+    img = cv2.resize(img, None, None, fx=1.0 / scale, fy=1.0 / scale, interpolation=cv2.INTER_LINEAR)
+    cv2.imwrite(os.path.join("data/results", base_name), img)
+
+
+def Gaussian_Blur(img, image_name, boxes, scale):
+    img = cv2.resize(img, None, None, fx=1.0 / scale, fy=1.0 / scale, interpolation=cv2.INTER_LINEAR)
+    hight, width = img.shape[0:2]
+    other_name = image_name.split('\\')[-1]
+    tmp = other_name.split('.')
+    base_name = tmp[0] + '_blur.' + tmp[1]
     for box in boxes:
         minY = min(int(box[1] / scale), int(box[3] / scale), int(box[5] / scale), int(box[7] / scale))
-        if minY > 0.92 * (hight/scale) and box[8] >= 0.9:
+        minX = min(int(box[0] / scale), int(box[2] / scale), int(box[4] / scale), int(box[6] / scale))
+        if minY > 0.92 * hight and box[8] >= 0.8 and minX > int(0.4*width):  #
             if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
                 continue
-            # if box[8] >= 0.9:
-            #     color = (0, 255, 0)
-            # elif box[8] >= 0.8:
-            #     color = (255, 0, 0)
-            color = (0, 255, 0)
-            cv2.line(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), color, 2)
-            cv2.line(img, (int(box[0]), int(box[1])), (int(box[4]), int(box[5])), color, 2)
-            cv2.line(img, (int(box[6]), int(box[7])), (int(box[2]), int(box[3])), color, 2)
-            cv2.line(img, (int(box[4]), int(box[5])), (int(box[6]), int(box[7])), color, 2)
-
             min_x = min(int(box[0] / scale), int(box[2] / scale), int(box[4] / scale), int(box[6] / scale))
             min_y = min(int(box[1] / scale), int(box[3] / scale), int(box[5] / scale), int(box[7] / scale))
             max_x = max(int(box[0] / scale), int(box[2] / scale), int(box[4] / scale), int(box[6] / scale))
             max_y = max(int(box[1] / scale), int(box[3] / scale), int(box[5] / scale), int(box[7] / scale))
+            img_mask = img[min_y:max_y, min_x:max_x]
+            blur = cv2.GaussianBlur(img_mask, (99, 99), 0)
+            img[min_y:max_y, min_x:max_x] = blur
 
-            # line = ','.join([str(min_x), str(min_y), str(max_x), str(max_y)]) + '\r\n'
-
-    img = cv2.resize(img, None, None, fx=1.0 / scale, fy=1.0 / scale, interpolation=cv2.INTER_LINEAR)
     cv2.imwrite(os.path.join("data/results", base_name), img)
 
 
@@ -64,6 +87,7 @@ def ctpn(sess, net, image_name):
     textdetector = TextDetector()
     boxes = textdetector.detect(boxes, scores[:, np.newaxis], img.shape[:2])
     draw_boxes(img, image_name, boxes, scale)
+    # Gaussian_Blur(img, image_name, boxes, scale)
     timer.toc()
     print(('Detection took {:.3f}s for '
            '{:d} object proposals').format(timer.total_time, boxes.shape[0]))
